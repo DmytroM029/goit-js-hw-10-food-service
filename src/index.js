4c4d67b5ae06f7408dd48669c50063f08feb6cb8
Dmytro Marchenko <arif007@ukr.net>
2021-04-30 15:23:47 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 import './sass/main.scss';
+import template from './template/main.hbs';
+import massive from './menu.json';
+
+// window.onload = () => {
+//     const container = document.getElementById("menu");
+// container.innerHTML = template(massive)};
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const classMenu = document.querySelector('.js-menu');
+const menuMarkup = template(massive);
+
+classMenu .insertAdjacentHTML('beforeend', menuMarkup);
+
+//Тема
+const themeChange = document.querySelector('.theme-switch__toggle');
+const body = document.body;
+
+themeChange.addEventListener('change', onThemeChange);
+
+function onThemeChange(event){
+    if (themeChange.checked) {
+		body.classList.add(`${Theme.DARK}`);
+        body.classList.remove(`${Theme.LIGHT}`);
+        localStorage.setItem('theme', `${Theme.DARK}`);
+	}
+	else {
+        body.classList.remove(`${Theme.DARK}`)
+		body.classList.add(`${Theme.LIGHT}`);
+        localStorage.setItem('theme', `${Theme.LIGHT}`);
+    }
+};
+
+function currentTheme(){
+    const savedTheme = localStorage.getItem('theme');
+
+    if(currentTheme === `${Theme.DARK}`){
+        themeSwitcher.checked = true;
+        body.classList.add(`${Theme.DARK}`);
+    }
+    else{
+        themeSwitcher.checked = false;
+        body.classList.add(`${Theme.LIGHT}`);
+    }
+};
+
+currentTheme();
